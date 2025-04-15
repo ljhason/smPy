a75bf72edecd5768dcd8af36525b83f53bc84f66
ljhason <lily.jian.hua.anderson@gmail.com>
2025-04-15 19:29:33 +0100

    Created 3 CH changes

--- src/smPy/smpy.py
+++ src/smPy/smpy.py
@@ -195,7 +195,6 @@ def apply_polyfit_params(CH1_peaks, params_x, params_y):
     return np.column_stack((y_mapped, x_mapped)) 
 
 
-
 def plot_circle(image, y_centre, x_centre, radius=4, colour = [255, 255, 0]):
     """
     Plots a circle on the image at the specified coordinates (y_centre, x_centre) with the specified radius and colour.
@@ -378,7 +377,7 @@ def interactive_2CH_plots(event, pma_file_path, fig, ax, scatter_data, y_centre_
 
                 ax_intensity_CH1.clear()
                 ax_intensity_CH1.plot(time, tot_intensity_all_frames_CH1, color='g', label='CH2')
-                ax_intensity_CH1.set_title(f"Intensity v Time in Donor Peak {idx}")
+                ax_intensity_CH1.set_title(f"Intensity v Time in Donor Peak {idx}, BT: {background_treatment}, CH consideration: {CH_consideration}")
                 ax_intensity_CH1.set_xlabel('Time (s)')
                 ax_intensity_CH1.set_ylabel('Intensity')
                 ax_intensity_CH1.set_ylim(-255, max(tot_intensity_all_frames_CH1)+255)
@@ -389,7 +388,7 @@ def interactive_2CH_plots(event, pma_file_path, fig, ax, scatter_data, y_centre_
 
                 ax_intensity_CH2.clear()
                 ax_intensity_CH2.plot(time, tot_intensity_all_frames_CH2, color='b', label='CH2')
-                ax_intensity_CH2.set_title(f"Intensity v Time in Acceptor Peak {idx}")
+                ax_intensity_CH2.set_title(f"Intensity v Time in Acceptor Peak {idx}, BT: {background_treatment}, CH consideration: {CH_consideration}")
                 ax_intensity_CH2.set_xlabel('Time (s)')
                 ax_intensity_CH2.set_ylabel('Intensity')
                 ax_intensity_CH2.set_ylim(-255, max(tot_intensity_all_frames_CH2)+255)
@@ -509,7 +508,7 @@ def interactive_2CH_plots_merged(event, pma_file_path, fig, ax, scatter_data, y_
                 ax_intensity.clear()
                 ax_intensity.plot(time, tot_intensity_all_frames_CH1, color='g', label='CH1')
                 ax_intensity.plot(time, tot_intensity_all_frames_CH2, color='b', label='CH2')
-                ax_intensity.set_title(f"Intensity v Time in Peak {idx}")
+                ax_intensity.set_title(f"Intensity v Time in Peak {idx}, BT: {background_treatment}, CH consideration: {CH_consideration}")
                 ax_intensity.set_xlabel('Time (s)')
                 ax_intensity.set_ylabel('Intensity')
                 ax_intensity.set_ylim(-255, max(max(tot_intensity_all_frames_CH1), max(tot_intensity_all_frames_CH2))+255)
@@ -588,7 +587,7 @@ def display_time_series(pma_file_path, avg_image, peak_idx, CH1_arr, CH2_arr, tp
     fig, ax = plt.subplots(4, 1, figsize=figsize)
     fig.subplots_adjust(hspace=1)
     ax[0].plot(time, tot_intensity_all_frames_CH1, color='b')
-    ax[0].set_title(f'Intensity v Time in Donor Peak {peak_idx}')
+    ax[0].set_title(f'Intensity v Time in Donor Peak {peak_idx}, BT: {background_treatment}, CH consideration: {CH_consideration}')
     ax[0].set_ylabel('Intensity')
     ax[0].set_xlabel('Time (s)')
     ax[0].set_ylim(-255, max(tot_intensity_all_frames_CH1)+255)
@@ -599,7 +598,7 @@ def display_time_series(pma_file_path, avg_image, peak_idx, CH1_arr, CH2_arr, tp
 
 
     ax[1].plot(time, tot_intensity_all_frames_CH2, color='g')
-    ax[1].set_title(f'Intensity v Time in Acceptor Peak {peak_idx}')
+    ax[1].set_title(f'Intensity v Time in Acceptor Peak {peak_idx}, BT: {background_treatment}, CH consideration: {CH_consideration}')
     ax[1].set_ylabel('Intensity')
     ax[1].set_xlabel('Time (s)')
     ax[1].set_ylim(-255, max(tot_intensity_all_frames_CH2)+255)
@@ -688,3 +687,161 @@ def find_trip(peaks_1, mapped_CH2, mapped_CH3, tolerance=4, shift_CH2=[0,0], shi
 
     return len(out_pair_arr_CH1), out_pair_arr_CH1, out_pair_arr_CH2, out_pair_arr_CH3
 
+
+
+def display_three_plots(event, pma_file_path, fig, ax, scatter_data, y_centre_arr, x_centre_arr, image_3d, image_orig, mask, radius=4, tpf=1/100, R_0_1=5.6, R_0_2=5.6, time_interval=10, intensity_interval=500, FRET_interval=0.2, distance_1_interval=0.5, distance_2_interval=0.2, zoom_size=5, background_treatment="None", CH_consideration=False, Intense_axes=[0.48, 0.81, 0.5, 0.15], FRET_axes = [0.48, 0.56, 0.5, 0.15], distance_axes_1=[0.48, 0.31, 0.5, 0.15], distance_axes_2=[0.48, 0.06, 0.5, 0.15],  CH1_zoom_axes=[0.0005, 0.05, 0.15, 0.15], CH2_zoom_axes=[0.135, 0.05, 0.15, 0.15], CH3_zoom_axes=[0.27, 0.05, 0.15, 0.15]):
+    """
+    From a pma file, displays the time series of the total intensity in a circle of a specified radius centred on coordinates (y_centre, x_centre) for all channels.
+    """
+    visible = False
+    for scatter, peaks, label in scatter_data:
+        cont, ind = scatter.contains(event)
+        if cont:
+            update_annot(ind, scatter, peaks, label)
+            visible = True
+            idx = ind['ind'][0]
+
+            if event.name == "button_press_event":
+                if background_treatment == "None":
+                    Frames_data = read_pma(pma_file_path)
+                elif background_treatment == "SG":
+                    Frames_data = SG_background_subtraction(pma_file_path, image_3d, radius, y_centre_arr, x_centre_arr, CH_consideration=CH_consideration)
+                elif background_treatment == "DG":
+                    Frames_data = DG_background_subtraction(pma_file_path, radius, y_centre_arr, x_centre_arr, CH_consideration=CH_consideration)
+                else:
+                    Frames_data = read_pma(pma_file_path)
+                    
+                for patch in ax.patches:
+                    patch.remove()
+                    
+                for ax_zoom in fig.axes:
+                    if ax_zoom is not ax:  # Keep the main axis
+                        fig.delaxes(ax_zoom)
+
+                ax_zoom_CH1 = fig.add_axes(CH1_zoom_axes)
+                ax_zoom_CH2 = fig.add_axes(CH2_zoom_axes)
+                ax_zoom_CH3 = fig.add_axes(CH3_zoom_axes)
+
+                ax_intensity = fig.add_axes(Intense_axes)
+                ax_FRET = fig.add_axes(FRET_axes)
+                ax_distance_1 = fig.add_axes(distance_axes_1)
+                ax_distance_2 = fig.add_axes(distance_axes_2)
+
+                y_CH1, x_CH1 = scatter_data[0][1][idx]
+                x1_CH1, x2_CH1 = max(0, x_CH1 - zoom_size), min(image_3d.shape[1], x_CH1 + zoom_size)
+                y1_CH1, y2_CH1 = max(0, y_CH1 - zoom_size), min(image_3d.shape[0], y_CH1 + zoom_size)
+                y_CH2, x_CH2 = scatter_data[1][1][idx]
+                x1_CH2, x2_CH2 = max(0, x_CH2 - zoom_size), min(image_3d.shape[1], x_CH2 + zoom_size)
+                y1_CH2, y2_CH2 = max(0, y_CH2 - zoom_size), min(image_3d.shape[0], y_CH2 + zoom_size)
+                y_CH3, x_CH3 = scatter_data[2][1][idx]
+                x1_CH3, x2_CH3 = max(0, x_CH3 - zoom_size), min(image_3d.shape[1], x_CH3 + zoom_size)
+                y1_CH3, y2_CH3 = max(0, y_CH3 - zoom_size), min(image_3d.shape[0], y_CH3 + zoom_size)
+
+                zoomed_image_CH1 = image_orig[y1_CH1:y2_CH1, x1_CH1:x2_CH1]
+                zoomed_image_CH2 = image_orig[y1_CH2:y2_CH2, x1_CH2:x2_CH2]
+                zoomed_image_CH3 = image_orig[y1_CH3:y2_CH3, x1_CH3:x2_CH3]
+
+                ax_zoom_CH1.clear()
+                ax_zoom_CH1.imshow(zoomed_image_CH1, cmap="gray")
+                ax_zoom_CH1.set_xticks([])
+                ax_zoom_CH1.set_yticks([])
+                ax_zoom_CH1.set_title(f"CH1: ({y_CH1}, {x_CH2})")
+                rect1 = patches.Rectangle((x1_CH1, y1_CH1), x2_CH1 - x1_CH1, y2_CH1 - y1_CH1, linewidth=1.5, edgecolor='g', facecolor='none')
+                ax.add_patch(rect1)
+                ax_zoom_CH2.clear()
+            
+                
+                ax_zoom_CH2.imshow(zoomed_image_CH2, cmap="gray")
+                ax_zoom_CH2.set_xticks([])
+                ax_zoom_CH2.set_yticks([])
+                ax_zoom_CH2.set_title(f"CH2: ({y_CH2}, {x_CH2})")
+                rect2 = patches.Rectangle((x1_CH2, y1_CH2), x2_CH2 - x1_CH2, y2_CH2 - y1_CH2, linewidth=1.5, edgecolor='b', facecolor='none')
+                ax.add_patch(rect2)
+
+                ax_zoom_CH3.clear()
+                ax_zoom_CH3.imshow(zoomed_image_CH3, cmap="gray")
+                ax_zoom_CH3.set_xticks([])
+                ax_zoom_CH3.set_yticks([])
+                ax_zoom_CH3.set_title(f"CH3: ({y_CH3}, {x_CH3})")
+                rect3 = patches.Rectangle((x1_CH3, y1_CH3), x2_CH3 - x1_CH3, y2_CH3 - y1_CH3, linewidth=1.5, edgecolor='purple', facecolor='none')
+                ax.add_patch(rect3)
+
+                tot_intensity_all_frames_CH1 = []
+                tot_intensity_all_frames_CH2 = []
+                tot_intensity_all_frames_CH3 = []
+
+                for i in range(len(Frames_data)):
+
+                    # transforms from 2D to 3D
+                    if Frames_data[i].ndim == 2:
+                        frame_data_3d = np.repeat(Frames_data[i][..., np.newaxis], 3, -1)
+                    elif Frames_data[i].ndim==3 and Frames_data[i].shape[2]==3:
+                        frame_data_3d = Frames_data[i]
+
+                    frame_data_copy = frame_data_3d.copy()
+                    frame_data_copy[mask] = [255, 255, 0]
+
+                    total_intensity_CH1,_ = intensity_in_circle(frame_data_3d, radius, y_CH1, x_CH1)
+                    total_intensity_CH2,_ = intensity_in_circle(frame_data_3d, radius, y_CH2, x_CH2)
+                    total_intensity_CH3,_ = intensity_in_circle(frame_data_3d, radius, y_CH3, x_CH3)
+                    tot_intensity_all_frames_CH1.append(total_intensity_CH1)
+                    tot_intensity_all_frames_CH2.append(total_intensity_CH2)
+                    tot_intensity_all_frames_CH3.append(total_intensity_CH3)
+
+                time = np.linspace(0, (len(tot_intensity_all_frames_CH1)-1) * tpf, len(tot_intensity_all_frames_CH1))
+
+                ax_intensity.clear()
+                ax_intensity.plot(time, tot_intensity_all_frames_CH1, color='g', label='CH1')
+                ax_intensity.plot(time, tot_intensity_all_frames_CH2, color='b', label='CH2')
+                ax_intensity.plot(time, tot_intensity_all_frames_CH3, color='purple', label='CH3')
+                ax_intensity.set_title(f"Intensity v Time in Peak {idx}, BT: {background_treatment}, CH consideration: {CH_consideration}")
+                ax_intensity.set_xlabel("Time (s)")
+                ax_intensity.set_ylabel("Intensity")
+                ax_intensity.set_xlim(0, time[-1])
+                ax_intensity.grid()
+                ax_intensity.legend(loc="upper right")
+                ax_intensity.xaxis.set_major_locator(MultipleLocator(time_interval))
+                ax_intensity.yaxis.set_major_locator(MultipleLocator(intensity_interval))
+
+                FRET_values_1 = calc_FRET(tot_intensity_all_frames_CH1, tot_intensity_all_frames_CH2)
+                FRET_values_2 = calc_FRET(tot_intensity_all_frames_CH2, tot_intensity_all_frames_CH3)           
+                ax_FRET.clear()
+                ax_FRET.plot(time, FRET_values_1, color='r', label='CH1-CH2')
+                ax_FRET.plot(time, FRET_values_2, color='c', label='CH2-CH3')
+                ax_FRET.set_title(f"FRET v Time (Triplet {idx})")
+                ax_FRET.set_xlabel("Time (s)")
+                ax_FRET.set_ylabel("Intensity")
+                ax_FRET.set_xlim(0, time[-1])
+                ax_FRET.grid()
+                ax_FRET.legend(bbox_to_anchor=(1, 1.4),loc="upper right")
+
+                ax_FRET.xaxis.set_major_locator(MultipleLocator(time_interval))
+                ax_FRET.yaxis.set_major_locator(MultipleLocator(FRET_interval))
+
+                distance_1 = calc_distance(FRET_values_1, R_0_1)
+
+                ax_distance_1.clear()
+                ax_distance_1.plot(time, distance_1, color='r')
+                ax_distance_1.set_title(f"CH1-CH2 Distance v Time (Peak ID: {idx})")
+                ax_distance_1.set_xlabel("Time (s)")
+                ax_distance_1.set_ylabel("Distance (nm)")
+                ax_distance_1.set_xlim(0, time[-1])
+                ax_distance_1.grid()
+                ax_distance_1.xaxis.set_major_locator(MultipleLocator(time_interval))
+                ax_distance_1.yaxis.set_major_locator(MultipleLocator(distance_1_interval))
+                
+                distance_2 = calc_distance(FRET_values_2, R_0_2)
+                ax_distance_2.clear()
+                ax_distance_2.plot(time, distance_2, color='c')
+                ax_distance_2.set_title(f"CH2-CH3 Distance v Time (Peak ID: {idx})")
+                ax_distance_2.set_xlabel("Time (s)")
+                ax_distance_2.set_ylabel("Distance (nm)")
+                ax_distance_2.set_xlim(0, time[-1])
+                ax_distance_2.grid()
+                ax_distance_2.xaxis.set_major_locator(MultipleLocator(time_interval))
+                ax_distance_2.yaxis.set_major_locator(MultipleLocator(distance_2_interval))
+
+    annot.set_visible(visible)
+    fig.canvas.draw_idle()
+
+    
